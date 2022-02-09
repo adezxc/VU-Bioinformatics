f82171303b04f1165448249660a4de3c5e5a3330
adezxc <adam.jasinski@unity3d.com>
2022-02-09 13:11:34 +0200

    yes

--- semester1/first.pl
+++ semester1/first.pl
@@ -5,6 +5,7 @@ use warnings;
 
 my @words = @ARGV;
 my $length = scalar(@words);
+my $longest = 0;
 
 print $length,"\n";
 
@@ -18,7 +19,10 @@ for (my $i = 0; $i < $length; $i++) {
     print $words[$i], " " if $i % 2 != 0;
 
     unless (exists $table[$i]) { push(@table, $words[$i]) }
+
+    longest = length($words[$i]) if length($words[$i]) > $longest;
 }
+
 print "\n";
 
 for (my $i = 0; $i < scalar(@table); $i++) {
