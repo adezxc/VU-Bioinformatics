f362cdf83c3fde364780705d405bfce64140b307
adezxc <adam.jasinski@unity3d.com>
2022-03-18 00:43:00 +0200

    finish task3

--- semester1/third.pl
+++ semester1/third.pl
@@ -10,34 +10,38 @@ $first = { value => '0', 'prev' => $first, 'next' => $first };
 
 my $pointer = $first;
 
-my $pos = 1;
+my $pos   = 1;
 my $count = 1;
 for (@elements) {
-    if ($pos > $count) {
+    if ( $pos > $count ) {
         $pos = 1;
-    } elsif ($pos < 0) {
+    }
+    elsif ( $pos < 0 ) {
         $pos = $count;
     }
     if ( $_ eq '-' ) {
         $pointer = $pointer->{'prev'};
         $pos--;
-    } elsif ( $_ eq '+' ) {
+    }
+    elsif ( $_ eq '+' ) {
         $pointer = $pointer->{'next'};
         $pos++;
-    } else {
-    my $elem = { value => $_, 'prev' => $pointer, 'next' => undef };
-    
-    if ($pos < $count) {
-        $elem->{'next'} = $pointer->{'next'};
-        $pointer->{'next'}->{'prev'} = $elem;
-    } else {
-        $first->{'prev'} = $elem;
-        $elem->{'next'} = $first;
     }
-    $pointer->{'next'} = $elem;
-    $pointer = $elem;
-    $count++;
-    $pos++;
+    else {
+        my $elem = { value => $_, 'prev' => $pointer, 'next' => undef };
+
+        if ( $pos < $count ) {
+            $elem->{'next'} = $pointer->{'next'};
+            $pointer->{'next'}->{'prev'} = $elem;
+        }
+        else {
+            $first->{'prev'} = $elem;
+            $elem->{'next'}  = $first;
+        }
+        $pointer->{'next'} = $elem;
+        $pointer = $elem;
+        $count++;
+        $pos++;
     }
 }
 
@@ -47,7 +51,7 @@ for ( ; ; ) {
     print $current->{value}, " ";
     $current = $current->{'next'};
 
-    if ($first == $current) {
+    if ( $first == $current ) {
         last;
     }
 }
