456d0efbae3bcbf3c268d2192c5296040b3986f2
adezxc <adam.jasinski@unity3d.com>
2022-04-07 22:54:35 +0300

    complete task7

--- semester1/7.pl
+++ semester1/7.pl
@@ -3,16 +3,69 @@
 use strict;
 use warnings;
 
-my @input;
 
-scalar(@ARGV) != 0 or die "You need to provide atleast one argument!";
-my $trim = $ARGV[1];
+die "You need to provide atleast one argument!" unless scalar(@ARGV) != 0;
+my $trim = $ARGV[0];
+my @chars = split(// , $trim); 
+my $trim_regex = '';
+my $trim_position = 0;
+my $position = 0;
+my ($product, @input, $head, $initial_position, );
+my $seq = '';
+
+my %fasta = (
+    "R" => '[AG]',
+    "Y" => '[CT]',
+    "K" => '[GT]',
+    "M" => '[AC]',
+    "S" => '[CG]',
+    "W" => '[AT]',
+    "B" => '[CGT]',
+    "D" => '[AGT]',
+    "H" => '[ACT]',
+    "V" => '[ACG]',
+    "N" => '[ACGT]',
+    "A" => '[A]',
+    "C" => '[C]',
+    "G" => '[G]',
+    "T" => '[T]',
+);
+
+for (my $i = 0; $i < length($trim); $i++ ) {
+    if ($chars[$i] ne '/') {
+        $trim_regex = $trim_regex . $fasta{$chars[$i]};
+    } else {
+        $trim_position = $i;
+    }
+}
 
 if ( scalar(@ARGV) != 1 ) {
-    @input = @ARGV;
+    @input = @ARGV[1..$#ARGV];
 } else {
-    print "Enter FASTA files that you want to analyze \n";
-    print "<Ctrl>-D to terminate \n";
     @input = <STDIN>;
+    chomp @input;
 }
- 
+my $i = 0;
+for (@input) {
+    open( my $file, '<', $_ ) or die $0, ':', $_, ':failed to open file:';
+    my $fasta = join( '', <$file> );
+    while ( $fasta =~ /(>[^\n]*)\n([^>]*)/g ) {
+        $head = $1;
+        $seq  = $2;
+        $seq =~ s/\R//g;
+        while ( $seq =~ /$trim_regex/g ) {
+            $initial_position = $position;
+            $position = "@+" + $trim_position - length($trim) + 1;
+            if ( $position - $initial_position != 0 ) {
+                print $head, " ", $position + 1, "\n";
+                $product = substr( $seq, $initial_position, $position - $initial_position );
+                print $product, "\n";
+            }
+        }
+        print $head, "\n";
+        print substr( $seq, $position, length($seq) - $position ), "\n";
+        $initial_position = $position = 0;
+    }
+    close $file;
+}
+
