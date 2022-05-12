81798bd4bc7c692b315ab59ec2afa4c9edf82a55
adezxc <adam.jasinski@unity3d.com>
2022-05-13 00:50:55 +0300

    finish ex 11

--- semester1/11.pl
+++ semester1/11.pl
@@ -2,13 +2,13 @@
 
 use strict;
 use warnings;
-use Data::Dumper;
 
 my $seq = '';
-my $IC = 0;
+my $ic = 0;
 my @matrix;
 
 while ( <> ) {
+    chomp $_;
     if ( $_ =~/^>.+/ ){
         if ($seq ne '') {
             AddFrequency($seq)
@@ -24,6 +24,7 @@ if ($seq ne '') {
 
 sub AddFrequency {
     my @seq = split("", shift);
+    
     for (my $i = 0; $i < scalar(@seq); $i++) {
         if ($seq[$i] ne "." || $seq[$i] ne "-" || $seq[$i] ne " ") {
             $matrix[$i]{"$seq[$i]"}++;
@@ -34,12 +35,16 @@ sub AddFrequency {
 for my $pos (@matrix) {
     my $sum;
     $sum += %$pos{$_} for keys %$pos;
+
     my %matrix_weights;
     $matrix_weights{$_} = %$pos{$_}/$sum for keys %$pos;
+
     my @sorted = sort { $pos->{$b} <=> $pos->{$a} || $a cmp $b} keys(%$pos);
     for my $key (@sorted) {
-        $IC += $matrix_weights{$key} * log($matrix_weights{$key})/log(2);
+        $ic += $matrix_weights{$key} * log($matrix_weights{$key})/log(2);
         printf "$key: %.2f ", $matrix_weights{$key} if ($matrix_weights{$key} >= 1/20);
     }
     print "\n";
 }
+
+printf "IC = %.2f\n", $ic += scalar(@matrix) * log(20)/log(2);
